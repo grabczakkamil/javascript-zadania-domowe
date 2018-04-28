11ca126a77c0866da7ed9bbba46338910cd321fe
Kamil Grabczak <grabczakkamil@gmail.com>
2018-04-28 09:43:04 +0200

    fibonacci przykład z zajęć

--- zadanie8124-element-fibonacci/js/index.js
+++ zadanie8124-element-fibonacci/js/index.js
@@ -12,3 +12,24 @@ function elementCiagu(n) {
 }
 elementCiagu();
 console.log(elementCiagu(2));
+
+
+/*przykład z zajęć*/
+function fibonacci(index) {
+    if (index === 0) {
+        console.log(0);
+        return;
+    }
+    if (index === 1) || index === 2 {
+        console.log(1);
+        return;
+    }
+    var currentValue = 1;
+    var previousValue = 1;
+    for (var i = 2; i<index; i++ ) {
+        var tempCurrentValue;
+        var currentValue = currentValue + previousValue;
+        var previousValue = tempCurrentValue;
+    }
+    console.log(currentValue);
+}
